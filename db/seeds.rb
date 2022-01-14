791fb4cebeb7170df98fc7e00b0b4469ad8ed546
Jackson Stark <jacksonstark77@hotmail.com>
2022-01-14 14:14:14 -0800

    seed articles table
    update seeds
    
    update seeder
    
    update seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# generate 20 articles
+(1..300).each do |id|
+    Article.create!(
+        id: id, 
+        title: Faker::Company.catch_phrase,
+        body: Faker::Lorem.paragraph_by_chars(number: 650),
+        image_url: Faker::LoremFlickr.image(size: "500x500", search_terms: ['nature']) + '?random=' + Faker::Number.number(digits: 5).to_s,
+        created_at: Faker::Date.between(from: 300.days.ago, to: Date.today)
+    )
+end
