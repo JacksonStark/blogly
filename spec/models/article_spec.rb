77a24664d9446ab67dbe6ecdb92abf82b26bcd94
Jackson Stark <jacksonstark77@hotmail.com>
2022-01-16 16:26:19 -0800

    add article model specs
    update article model specs

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:article) {
+        Article.new(
+            title: "Test title",
+            body: "Test body",
+            image_url: 'www.test.com/test.jpg'
+        )
+    }
+
+    it "is valid with all parameters" do
+        expect(article).to be_valid
+    end
+
+    it "is valid with only required parameters" do
+        article.body = nil
+        article.image_url = nil
+        expect(article).to be_valid
+    end
+
+    it "is invalid without required parameters" do
+        article.title = nil
+        expect(article).to_not be_valid
+    end
 end
