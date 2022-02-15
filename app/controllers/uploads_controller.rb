8bf73ec4c5fa8c6481ae4d5a777428d312478854
Jackson Stark <jacksonstark77@hotmail.com>
2022-02-15 00:23:20 -0800

    abstract out UploadService

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,20 +1,11 @@
-require 'aws-sdk-s3'
-require 'securerandom'
-
 class UploadsController < ApplicationController
     def presigned_url
-        s3 = Aws::S3::Client.new(
-            region: 'us-west-2',
-            access_key_id: Rails.application.credentials.config[:aws][:access_key_id],
-            secret_access_key: Rails.application.credentials.config[:aws][:secret_access_key],
-        )
+        filename = SecureRandom.hex(10) # for obfuscation purposes
+        upload_service = Services::UploadService.new
         
-        signer = Aws::S3::Presigner.new(client: s3)
-        filename = "#{SecureRandom.hex(10)}.png"
-        presigned_url, headers = signer.presigned_request(:put_object, bucket: "bl0gly", key: filename)
-        download_url = "https://bl0gly.s3.us-west-2.amazonaws.com/#{filename}"
+        presigned_url = upload_service.get_presigned_url(filename)
+        download_url = upload_service.get_download_url(filename)
 
-        render json: {presigned_url: presigned_url, download_url: download_url}, content_type: "application/json"
+        render json: {presigned_url: presigned_url, download_url: download_url}
     end
 end
-
