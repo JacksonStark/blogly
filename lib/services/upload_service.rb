c8b5d2e518d4cd370a50c476594109efdcc7e94d
Jackson Stark <jacksonstark77@hotmail.com>
2022-02-19 23:01:50 -0800

    add upload service specs (#3)
    update CI
    
    update upload service
    
    refactor upload service

--- lib/services/upload_service.rb
+++ lib/services/upload_service.rb
@@ -3,23 +3,25 @@ require 'securerandom'
 
 module Services
     class UploadService
-        def initialize()
-            @client = make_s3_client
+        attr_accessor :client
+
+        def initialize(client = make_s3_client, filename = SecureRandom.hex(10))
+            @client = client
+            @filename = filename
         end
 
-        def get_download_url(filename)
-            "https://#{ENV['AWS_S3_BUCKET']}.s3.#{ENV['AWS_S3_REGION']}.amazonaws.com/#{filename}"
+        def get_download_url
+            "https://#{ENV['AWS_S3_BUCKET']}.s3.#{ENV['AWS_S3_REGION']}.amazonaws.com/#{@filename}"
         end
         
-        def get_presigned_url(filename)
+        def get_presigned_url
             signer = make_s3_presigner
-            presigned_url, headers = signer.presigned_request(:put_object, bucket: ENV['AWS_S3_BUCKET'], key: filename)
+            presigned_url, headers = signer.presigned_request(:put_object, bucket: ENV['AWS_S3_BUCKET'], key: @filename)
             presigned_url
         end
 
         private
-        
-            def make_s3_client()
+            def make_s3_client
                 Aws::S3::Client.new(
                     region: ENV['AWS_S3_REGION'],
                     access_key_id: Rails.application.credentials.config[:aws][:access_key_id],
@@ -27,7 +29,7 @@ module Services
                 )
             end
 
-            def make_s3_presigner()
+            def make_s3_presigner
                 Aws::S3::Presigner.new(client: @client)
             end
     end
