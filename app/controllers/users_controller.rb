2e595fbb06e6734403da2ce88e7c4d7b70106e65
Jackson Stark <jacksonstark77@hotmail.com>
2022-02-20 20:50:24 -0800

    add users request flow

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to articles_url, notice: "User was successfully created."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def new
+        @user = User.new
+    end
+
+    private
+
+        def user_params
+            params.require(:user).permit(
+                :email,
+                :password,
+                :password_confirmation
+            )
+        end
+end
