aedce982d9ab9803f78111542a84bf72ea510a0d
Jackson Stark <jacksonstark77@hotmail.com>
2022-10-12 14:19:51 -0700

    abstract out upload handler

--- app/javascript/packs/tinymce.js
+++ app/javascript/packs/tinymce.js
@@ -1,21 +1,22 @@
-import { imagesUploadHandler } from '../utils/tinymce_uploader'
+import { tinyMceUploadHandler } from "../utils/uploader";
 
 export function initTinyMce() {
-    tinymce.init({
-        selector: '.tinymce',
-        height: 350,
-        menubar: false,
-        plugins: [
-            'image',
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-        ],
-        toolbar: 'file edit view insert format tools undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help | image',
-        content_style: "body {font-size: 11pt; color: #374151}",
-        skin: 'oxide-dark',
-        automatic_uploads: true,
-        paste_data_images: true,
-        images_upload_handler: imagesUploadHandler
-    });
+  tinymce.init({
+    selector: ".tinymce",
+    height: 350,
+    menubar: false,
+    plugins: [
+      "image",
+      "advlist autolink lists link image charmap print preview anchor",
+      "searchreplace visualblocks code fullscreen",
+      "insertdatetime media table paste code help wordcount",
+    ],
+    toolbar:
+      "file edit view insert format tools undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help | image",
+    content_style: "body {font-size: 11pt; color: #374151}",
+    skin: "oxide-dark",
+    automatic_uploads: true,
+    paste_data_images: true,
+    images_upload_handler: tinyMceUploadHandler,
+  });
 }
--- app/javascript/utils/uploader.js
+++ app/javascript/utils/uploader.js
@@ -1,13 +1,17 @@
-export async function imagesUploadHandler(blobInfo, success, failure, progress) {
+export async function tinyMceUploadHandler(blobInfo, success, failure, progress) {
+    const file = blobInfo.blob()
+    imagesUploadHandler(file, success, failure, progress)
+}
+
+export async function imagesUploadHandler(file, success, failure, progress) {
     // fetch presigned url for direct browser upload to S3
     const res = await fetch('/uploads/presigned_url', {method: 'GET'});
     const { presigned_url, download_url } = await res.json();
 
     // upload the file to S3 using presigned url
     try {
-        const file = blobInfo.blob()
         await fetch(presigned_url, {method: 'PUT', body: file});
-        success(download_url)
+        success(download_url) 
     } catch (err) {
         failure(`Image upload failed: ${err.message}`)
     }
