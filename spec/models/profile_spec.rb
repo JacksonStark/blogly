f0a2b2ff01e5d05a65f4ee45a4cad1fcfcd93023
Jackson Stark <jacksonstark77@hotmail.com>
2022-10-21 12:51:09 -0600

    add profile model specs

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "is associated with a user" do
+        user = create :user_with_all_attributes
+        profile = user.profile
+
+        expect(profile).to be_valid
+    end
 end
